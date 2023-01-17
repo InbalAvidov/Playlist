3fd57815fdf2f08b7ff715f7e2fbf47fe1b21e08
Inbal Avidov <inbal.avidov@gmail.com>
2023-01-17 18:02:51 +0200

    Project's basis

---
+++ src/root-cmp.jsx
@@ -0,0 +1,21 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import { store } from './store/store'
+import { AppHeader } from './cmps/app-header'
+import { UserMsg } from './cmps/user-msg'
+import { AppNav } from './cmps/app-nav'
+
+export function App() {
+    return (
+        <Provider store={store}>
+            <Router>
+                <section className="main-layout app">
+                    <AppHeader />
+                    <AppNav />
+                    <UserMsg />
+                </section>
+            </Router>
+        </Provider>
+    )
+}
