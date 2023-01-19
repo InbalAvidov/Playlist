a750f28c23955b52c01a8868a1821489871006fb
HazanOmri <omrihazan1313@gmail.com>
2023-01-19 15:26:25 +0200

    Merging

--- src/cmps/media-player-bar.jsx
+++ src/cmps/media-player-bar.jsx
@@ -5,24 +5,24 @@ import { useSelector } from "react-redux"
 
 export function MediaPlayerBar() {
     const songId = useSelector(storeState => storeState.playerModule.songId)
-    console.log(songId);
-    // const player = {
-    //     _id: 'RgKAFK5djSk',
-    //     title: 'See You Again',
-    //     cover,
-    //     artist: 'Wiz Khalifa'
-    // }
     // const player = useSelector(storeState => storeState.playerModule.player)
+    console.log(songId);
+    const player = {
+        _id: 'RgKAFK5djSk',
+        title: 'See You Again',
+        cover,
+        artist: 'Wiz Khalifa'
+    }
 
     return (<div className="media-player" >
-        <Soundplayer playerId={songId} />
-        <div className="information">
-            <img src={cover} alt="no image" />
+        <Soundplayer playerId={player._id} />
+        {player && <div className="information">
+            <img src={player.cover} alt="no image" />
             <div className="details">
                 <h4>title</h4>
                 <h5>artist</h5>
             </div>
-        </div>
+        </div>}
         <MediaConroller />
     </div>
     )
