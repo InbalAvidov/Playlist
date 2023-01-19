fa320bd902fb18dc415161f0d902f89c2e9863bc
HazanOmri <omrihazan1313@gmail.com>
2023-01-19 12:42:59 +0200

    Merging

--- src/cmps/media-controller.jsx
+++ src/cmps/media-controller.jsx
@@ -9,9 +9,9 @@ export function MediaConroller() {
     const player = useSelector(storeState => storeState.playerModule.player)
     const [isPlaying, setIsPlaying] = useState(false)
     const [currentTime, setCurrentTime] = useState('0:00')
-        const timerID = setInterval(() => {
-            setCurrentTime(utilService.secondsMinutesAndSeconds(player.getCurrentTime()))
-        }, 1000);
+    const timerID = setInterval(() => {
+        setCurrentTime(utilService.secondsMinutesAndSeconds(player.getCurrentTime()))
+    }, 1000);
     function togglePlay({ target }) {
         if (isPlaying) {
             player.pauseVideo()
@@ -23,19 +23,19 @@ export function MediaConroller() {
         // console.log(player)
         setIsPlaying(prevIsPlaying => !prevIsPlaying)
     }
-    return <div className="player">
-        <div className="player-btns">
+    return <div className="controller">
+        <div className="conrtoller-btns">
             {!isPlaying && <FontAwesomeIcon icon={faPlayCircle} onClick={togglePlay} />}
             {isPlaying && <FontAwesomeIcon icon={faPauseCircle} onClick={togglePlay} />}
         </div>
         <div className="player-progress-container">
             {player ? <p>{currentTime}</p> : <p>0:00</p>}
-            <input
-                type="range"
-                name="player-progress"
-                id="player-progress"
-                className="player-progress"
-            />
+                <input
+                    type="range"
+                    name="player-progress"
+                    id="player-progress"
+                    className="player-progress"
+                />
             {player ? <p>{utilService.secondsMinutesAndSeconds(player.getDuration())}</p> : <p>0:00</p>}
         </div>
 
