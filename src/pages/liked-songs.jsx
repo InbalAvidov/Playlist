78a539906f783bc8c6ceac3c1f4e65f71a98f6ff
hilashor <hilashor@gmail.com>
2023-01-21 15:41:00 +0200

    draft version of liked songs

--- src/pages/liked-songs.jsx
+++ src/pages/liked-songs.jsx
@@ -1,40 +1,53 @@
-
+import { useEffect } from "react"
+import { useSelector } from "react-redux"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-import { AppHeader } from "../cmps/app-header"
+import { faClock } from '@fortawesome/free-solid-svg-icons'
 
+import { loadStations } from "../store/station.actions"
+import { Loader } from "../cmps/loader"
+import { SongPreview } from "../cmps/station-song-preview"
 
+export function LikedSongs() {
+  const stations = useSelector((storeState) => storeState.stationModule.stations)
+  const user = useSelector((storeState => storeState.userModule.user))
 
+  console.log('LikedSongs user', user)
+  console.log('LikedSongs stations', stations)
 
-export function LikedSongs(){
+  useEffect(() => {
+      loadStations()
+  }, [])
 
+  if (!user) {
+    return (
+      <main className="liked-songs">
+        <h1>Liked Songs</h1>
+        <h4>Please login first</h4>
+      </main>
+    )
+  }
 
+  if (!stations) return <Loader />
+
+  const likedSongs = stations.flatMap(station => 
+    station.songs.filter(song => 
+      (song.likedByUsers || []).find(minimalUser => minimalUser._id === user._id)))
+
+      console.log('LikedSongs likedSongs', likedSongs)
   return (
-    <>
-    
-    <section className="liked-songs">
-        <section className="login-signup">
-        </section>
-
-        <section className="liked-header">
-          <div className="liked-header-details">
-            <p>PLAYLIST</p>
-            <h2>Liked Songs</h2>
-            <div className="loged-user-liked-songs">
-              <p>{}</p>
-            </div>
-
+    <main className="liked-songs">
+      <h1>Liked Songs</h1>
+      <section className="songs-list">
+          <div className="song-preview songs-list-header">
+              <p className="song-number">#</p>
+              <p className="song-img-title">Title</p>
+              <p className="song-date">Date Added</p>
+              <p className="song-duration"><FontAwesomeIcon icon={faClock}></FontAwesomeIcon></p>
           </div>
-          <div className="img-wrapper">
-            <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
-          </div>
-        </section>
-
-        <section className="main-liked-songs">
-          <h2>Liked song</h2>
-        </section>
+          {likedSongs.map((song, idx) =>
+              <SongPreview key={idx} song={song} idx={idx} />)
+          }
       </section>
-      
-    </>
+    </main>
   )
 }
