c57ecf04111beb5b95e2e3d5cb2c83212a64010f
hilashor <hilashor@gmail.com>
2023-01-24 13:09:40 +0200

    Added a description to playlists on the home page

--- src/cmps/rest-section-stations.jsx
+++ src/cmps/rest-section-stations.jsx
@@ -7,6 +7,21 @@ import defaultPhoto from '../assets/img/default-photo.jpeg'
 export function RestSectionStations({ stations }) {
     const player = useSelector(storeState => storeState.playerModule.player)
     console.log('PLAYER IM RestSectionStations CMP', player)
+
+    // function getStationDescription(station){
+    //     const channelTitles = station.songs.map(song => song.channelTitle);
+    //     const uniqueTitles = [...new Set(channelTitles)];
+    //     return uniqueTitles.slice(0,2).join(' • ');
+    // }
+    function getStationDescription(station){
+        const channelTitles = station.songs.map(song =>song.channelTitle);
+        const uniqueTitles = [...new Set(channelTitles)];
+        let combinedTitle = uniqueTitles.slice(0,2).join(' • ');
+        if(combinedTitle.length > 27) combinedTitle = combinedTitle.slice(0,27) + "..";
+        return combinedTitle;
+    }
+
+
     return (
         <section
             className="rest-section-stations">
@@ -23,8 +38,10 @@ export function RestSectionStations({ stations }) {
                     </div>
                     <h3>{station.name}</h3>
                     <p>{station.description?.slice(0, 15)}</p>
+                    <p>{getStationDescription(station)}</p>
                 </div>
             </Link>)}
         </section>
     )
 }
+
