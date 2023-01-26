eb7251570bbbea5d4e283141ef618472fc44999f
HazanOmri <omrihazan1313@gmail.com>
2023-01-26 09:22:57 +0200

    Flexibility design

--- src/cmps/rest-section-stations.jsx
+++ src/cmps/rest-section-stations.jsx
@@ -38,23 +38,19 @@ export function RestSectionStations({ stations }) {
             className="rest-section-stations">
             {stations.map(station => <Link to={`/station/${station._id}`} key={station._id}>
                 <div className="rest-section-station-preview" >
-                    <div className="img-container" style=
-                        {{
-                            backgroundImage: `url("${station.imgUrl ? station.imgUrl : station.songs.length > 0 ? station.songs[0].imgUrl : defaultPhoto}")`,
-                            backgroundRepeat: "repeat",
-                            backgroundPosition: "center",
-                            backgroundSize: "cover",
-                            width: '190px', height: '190px', margin: 'auto'
-                        }}
-                    >
-                        <button className='green-play-pause-btn' onClick={(event) => onPlayStation(event, station)}>
+                    <div className="img-container" >
+                        <img src={station.imgUrl ? station.imgUrl : station.songs.length > 0 ? station.songs[0].imgUrl : defaultPhoto} alt="" />
+                        <span className='green-play-pause-btn' onClick={(event) => onPlayStation(event, station)}>
                             <svg role="img" height="24" width="24" aria-hidden="true" viewBox="0 0 24 24" data-encore-id="icon" className="Svg-sc-ytk21e-0 uPxdw"><path d="M7.05 3.606l13.49 7.788a.7.7 0 010 1.212L7.05 20.394A.7.7 0 016 19.788V4.212a.7.7 0 011.05-.606z"></path></svg>
-                        </button>
+                        </span>
                     </div>
-                    <h3>{station.name}</h3>
-                    <p>{station.description?.slice(0, 15)}</p>
-                    <p>{getStationDescription(station)}</p>
+                    <div className='description'>
+                        <h3>{station.name}</h3>
+                        <p>{station.description?.slice(0, 15)}</p>
+                        <p>{getStationDescription(station)}</p>
+                    </div>
                 </div>
+                {/* </div> */}
             </Link>)}
         </section>
     )
