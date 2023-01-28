dd35da87f1d8edbca6d0aa2a6b6fca6185f9c9e3
Inbal Avidov <inbal.avidov@gmail.com>
2023-01-28 19:55:18 +0200

    Added svg to search page

--- src/pages/search-songs.jsx
+++ src/pages/search-songs.jsx
@@ -47,11 +47,11 @@ export function SearchSongs({ onAddSong, isForStation, scroll }) {
             setSongsBySearch(null)
             return
         }
-        try{
+        try {
             const results = await YoutubeService.getYoutubeReasults(val)
             setSongsBySearch(results)
-        }catch(err){
-            console.log('err:',err)
+        } catch (err) {
+            console.log('err:', err)
         }
     }
 
@@ -79,6 +79,7 @@ export function SearchSongs({ onAddSong, isForStation, scroll }) {
     return (
         <main className={`main-search ${isForStation ? '' : 'padding'}`} >
             <div className={`search-container ${isForStation ? 'station' : 'main-page'}`}>
+                <svg fill={isForStation ? 'grey' : 'black'} role='img' height={isForStation ? '20' : '24'} width={isForStation ? '20' : '24'} aria-hidden='true' className='Svg-sc-ytk21e-0 uPxdw search-icon' viewBox='0 0 24 24' data-encore-id='icon'><path d='M10.533 1.279c-5.18 0-9.407 4.14-9.407 9.279s4.226 9.279 9.407 9.279c2.234 0 4.29-.77 5.907-2.058l4.353 4.353a1 1 0 101.414-1.414l-4.344-4.344a9.157 9.157 0 002.077-5.816c0-5.14-4.226-9.28-9.407-9.28zm-7.407 9.279c0-4.006 3.302-7.28 7.407-7.28s7.407 3.274 7.407 7.28-3.302 7.279-7.407 7.279-7.407-3.273-7.407-7.28z'></path></svg>
                 <input className='search-input'
                     type='txt' value={search}
                     placeholder={isForStation ? 'Search for songs' : 'What do you want to listen to?'}
