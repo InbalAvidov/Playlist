aeeb83acbce0f3df6a9a58531391bbd4abd5d9a7
HazanOmri <omrihazan1313@gmail.com>
2023-01-30 12:23:32 +0200

    Merging

--- frontend/src/cmps/loader.jsx
+++ frontend/src/cmps/loader.jsx
@@ -1,22 +1,12 @@
 export function Loader({className}) {
     return (
-<<<<<<< HEAD
-        <div className="loader">
+        <div className={`loader ${className}`}>
             <div className="bar1"></div>
             <div className="bar2"></div>
             <div className="bar3"></div>
             <div className="bar4"></div>
             <div className="bar5"></div>
             <div className="bar6"></div>
-=======
-        <div className={`loader ${className}`}>
-            <div className='bar1'></div>
-            <div className='bar2'></div>
-            <div className='bar3'></div>
-            <div className='bar4'></div>
-            <div className='bar5'></div>
-            <div className='bar6'></div>
->>>>>>> refs/remotes/origin/main
         </div>
     )
 }
